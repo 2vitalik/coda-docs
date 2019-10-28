740546c50ca45e0d8f48e802c9bb295cfbcb7a90
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-10-29 00:19:18 +0200

    Try to bug-fix path issue

--- coda/coda.py
+++ coda/coda.py
@@ -1,11 +1,11 @@
-from coda.util import read_yaml
+from coda.util import read_yaml, root_dir
 
 
 class Coda:
     def __init__(self, doc_name, token):
         self.doc_name = doc_name
         self.token = token
-        self._data = read_yaml(f'coda/{self.doc_name}.yaml')
+        self._data = read_yaml(f'{root_dir}/coda/{self.doc_name}.yaml')
         self.doc_id = self._data['doc_id']
         self.telegram_chat = self._data['telegram_chat']
         self._tables = self._data['tables']
--- coda/util.py
+++ coda/util.py
@@ -1,4 +1,8 @@
+import os
+
 import yaml
+
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def read_yaml(filename):
