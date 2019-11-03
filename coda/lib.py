ff5ea71b6d7a8fbd606f2f61475e19200a51d3f8
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-11-03 22:52:57 +0200

    Rename `data` folder to `yaml`

--- coda/lib.py
+++ coda/lib.py
@@ -8,7 +8,7 @@ class Coda:
     def __init__(self, doc_name, token=None):
         self.doc_name = doc_name
         self.token = token
-        self._data = read_yaml(f'{root_dir}/data/{self.doc_name}.yaml')
+        self._data = read_yaml(f'{root_dir}/yaml/{self.doc_name}.yaml')
         self.doc_id = self._data['doc_id']
         self.telegram_chat = self._data['telegram_chat']
         self._tables = self._data['tables']
