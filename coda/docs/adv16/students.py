8660baa28911ba0a948577dc793e4d3ccce02a10
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-11-14 01:31:54 +0200

    Add couple classes for adv16 (lessons and students)

---
+++ coda/docs/adv16/students.py
@@ -0,0 +1,26 @@
+from coda.docs.adv18.mixins.indexer import IndexerMixin
+from coda.lib import Coda
+
+
+class Students(IndexerMixin):  # todo: some common base class?
+    def __init__(self, coda_token, skip_load_data=False):  # todo: default: True?
+        self.coda = Coda('adv16', coda_token)
+        self.table = self.coda.students
+        self.data = self.load_data() if not skip_load_data else None
+        self.indexes = {}  # todo: move to indexer mixin
+        self.add_index('telegram_id')
+
+    def load_data(self):
+        return self.table.rows_dict()
+
+    def reload_data(self):
+        self.data = self.load_data()
+
+    def value(self, field, row_id):
+        return self.data.get(row_id, {}).get(field)
+
+    def by_telegram_id(self, telegram_id):
+        return self.find('telegram_id', telegram_id)
+
+    def update(self, row_id, changes):  # todo: move to common base class
+        self.table.update(row_id, changes)
