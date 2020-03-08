43083dc3cff6fbd30cadd516d8fed4f2874a0930
Vitaliy Lyapota <2vitalik@gmail.com>
2020-03-08 15:39:19 +0200

    Refactoring for the script (more convenient usage)

--- coda/init/create_yaml.py
+++ coda/init/create_yaml.py
@@ -5,7 +5,7 @@ from coda.conf.coda_token import coda_token
 from coda.util import root_dir, read_yaml, write_yaml
 
 
-def first_step(semester, doc_name, doc_id, telegram_chat):
+def create_tables(semester, doc_name, doc_id, telegram_chat):
     yaml_path = f'{root_dir}/yaml/{semester}/{doc_name}.yaml'
     write(yaml_path, f'')
     append(yaml_path, f'telegram_chat: {telegram_chat}')
@@ -18,7 +18,7 @@ def first_step(semester, doc_name, doc_id, telegram_chat):
         append(yaml_path, f"    table_id: {table['id']}")
 
 
-def second_step(semester, doc_name):
+def update_tables(semester, doc_name):
     yaml_path = f'{root_dir}/yaml/{semester}/{doc_name}.yaml'
     data = read_yaml(yaml_path)
     doc_id = data['doc_id']
@@ -36,16 +36,20 @@ def second_step(semester, doc_name):
 if __name__ == '__main__':
     semester = '2020-1'
 
-    # doc_name = 'adv18'
-    # doc_id = 'PlGYEhaEib'
-    # telegram_chat = -399927652
-    # first_step(semester, doc_name, doc_id, telegram_chat)
-    # # rename table names manually
-    # second_step(semester, doc_name)
+    docs = [
+        {
+            'doc_name': 'adv18',
+            'doc_id': 'PlGYEhaEib',
+            'telegram_chat': -399927652,
+        },
+        {
+            'doc_name': 'adv17',
+            'doc_id': 'zqT1QhwbTC',
+            'telegram_chat': -291308716,
+        },
+    ]
 
-    # doc_name = 'adv17'
-    # doc_id = 'zqT1QhwbTC'
-    # telegram_chat = -291308716
-    # first_step(semester, doc_name, doc_id, telegram_chat)
-    # # rename table names manually
-    # second_step(semester, doc_name)
+    for doc in docs:
+        doc_name, doc_id, telegram_chat = doc.values()
+        # create_tables(semester, doc_name, doc_id, telegram_chat)
+        update_tables(semester, doc_name)
