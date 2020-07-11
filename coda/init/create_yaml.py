82eec58ffc18c0c6acec7e91140ec67dd103f42b
Vitaliy Lyapota <2vitalik@gmail.com>
2020-07-11 22:49:12 +0300

    Minor fix in `create_yaml.py` script

--- coda/init/create_yaml.py
+++ coda/init/create_yaml.py
@@ -34,32 +34,40 @@ def update_tables(semester, doc_name):
 
 
 if __name__ == '__main__':
-    semester = '2020-1'
-
     docs = [
         {
+            'semester': '2020-1',
             'doc_name': 'adv18',
             'doc_id': 'PlGYEhaEib',
             'telegram_chat': -399927652,
         },
         {
+            'semester': '2020-1',
             'doc_name': 'adv17',
             'doc_id': 'zqT1QhwbTC',
             'telegram_chat': -291308716,
         },
         {
+            'semester': '2020-1',
             'doc_name': 'adv16',
             'doc_id': 'aazaYSoRBc',
             'telegram_chat': -357420276,
         },
         {
+            'semester': '2020-1',
             'doc_name': 'oop',
             'doc_id': 'TdQAh6nS1P',
+            'telegram_chat': -436357091,
+        },
+        {
+            'semester': '2020-1',
+            'doc_name': 'adv18_data',
+            'doc_id': 'xEcFwErxgY',
             'telegram_chat': -436357091,
         },
     ]
 
     for doc in docs:
-        doc_name, doc_id, telegram_chat = doc.values()
+        semester, doc_name, doc_id, telegram_chat = doc.values()
         # create_tables(semester, doc_name, doc_id, telegram_chat)
         update_tables(semester, doc_name)
