c3cdeab6376afd24bc58ddf1e189409eb37b7f14
Vitaliy Lyapota <2vitalik@gmail.com>
2021-06-17 16:15:45 +0300

    v.0.5.4: Load latest from coda: `adv19`

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="coda-docs",
-    version="0.5.3",
+    version="0.5.4",
     author="Vitalik",
     author_email="2vitalik@gmail.com",
     description="Coda data for different projects",
